8ea4f7a8e3a122d4824fdf2b1cac29ebdf3e10b9
Vansh Patel <vanshp@dosepack.com>
2025-02-20 22:47:29 +0530

    implemented cold email generator

---
+++ chains.py
@@ -0,0 +1,57 @@
+import os
+import uuid
+
+from langchain_core.exceptions import OutputParserException
+from langchain_groq import ChatGroq
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0,model="llama-3.3-70b-versatile",
+                   api_key=os.getenv("GROQ_API_KEY"))
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            I am a software engineer  with 2 years of experience in Python, FastAPI, SQLAlchemy, CherryPy, and Peewee, with 
+            a strong background in API development, database structuring, payment integrations, real-time solutions using 
+            WebSockets, and migration automation. I have also worked on optimizing complex algorithms and developed an
+            appointment manager system that helps reduce waiting times.
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
